bd03ec016c3dd6d1389ffa1520ebe67df6f99a10
Katarzyna <noworyta.katarzyna@wp.pl>
2023-05-16 14:58:35 +0200

    pizza entity, model

---
+++ src/main/java/pl/wszib/pizzamarket/data/entities/PizzaEntity.java
@@ -0,0 +1,55 @@
+package pl.wszib.pizzamarket.data.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="pizzas")
+public class PizzaEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private String ingredients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+}
---
+++ src/main/java/pl/wszib/pizzamarket/services/PizzaService.java
@@ -0,0 +1,26 @@
+package pl.wszib.pizzamarket.services;
+
+import org.springframework.stereotype.Service;
+import pl.wszib.pizzamarket.data.entities.PizzaEntity;
+import pl.wszib.pizzamarket.data.repositories.PizzaRepository;
+import pl.wszib.pizzamarket.web.mappers.PizzaMapper;
+import pl.wszib.pizzamarket.web.models.PizzaModel;
+
+import java.util.List;
+
+@Service
+public class PizzaService {
+
+    private final PizzaRepository pizzaRepository;
+
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }
+    
+    public List<PizzaModel> findAll(){
+        List<PizzaEntity> entities = pizzaRepository.findAll();
+        return entities.stream()
+                .map(e -> PizzaMapper.toModel(e))//map(e -> PizzaMapper.toModel(e) - replace with method reference
+                .toList();
+    }
+}

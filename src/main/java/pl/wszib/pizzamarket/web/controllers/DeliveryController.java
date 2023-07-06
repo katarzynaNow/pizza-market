15c3814c95fbd607e0336fe058bac7f5bfd4e6d3
Katarzyna <noworyta.katarzyna@wp.pl>
2023-07-06 11:13:14 +0200

    delivery page

---
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/DeliveryController.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DeliveryController {
+
+    @GetMapping("delivery")
+    public String deliveryPage(){
+        return "deliveryPage";
+    }
+}
